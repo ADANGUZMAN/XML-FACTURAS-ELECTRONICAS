ade6ebd1a4892f5ebb5da82dec9da6de1ece094b
ADANGUZMAN <adan.guzman@epn.edu.ec>
2017-08-01 15:34:02 -0500

    VERSION 2.1
    MEJORAMIENTO EN DETALLES DE INTERFACES Y VALIDACION DE CAMPOS

---
+++ version 2.0/facturaElectronica.V2.1/FacturacionElectronica2.1/src/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+
+import guis.Login;
+import javafx.stage.Stage;
+import utils.Viewer;
+import utils.XMLTreeViewer;
+import utils.XMLViewer;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Login app=new Login();
+		app.setVisible(true);
+		//XMLTreeViewer ap=new XMLTreeViewer();
+		//ap.createUI();
+		
+	}
+
+}
